bc99a80421af0ac0bd3b689d44d3f0d6b16c1349
Gavin Keirstead <gmk1072@g.rit.edu>
2018-03-22 08:13:38 -0400

    pushing up for adding the environment variable to the new heroku app

--- server/app.js
+++ server/app.js
@@ -7,16 +7,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-  if (err) {
-    console.log('could not connect to database');
-    throw err;
-  }
+    if (err) {
+        console.log('could not connect to database');
+        throw err;
+    }
 });
+
+let redisURL = {
+    hostname: 'localHost',
+    port: 6379,
+};
+
+let redisPASS;
+
+if (process.env.REDISCLOUD_URL) {
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    redisPASS = redisURL.auth.split(':')[1];
+}
 
 const router = require('./router.js');
 
@@ -25,10 +39,15 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-  extended: true,
+    extended: true,
 }));
 app.use(session({
     key: 'sessionid',
+    store: new RedisStore({
+        host: redisURL.hostname,
+        port: redisURL.port,
+        pass: redisPASS,
+    }),
     secret: 'Domo Arigato',
     resave: true,
     saveUninitialized: true,
@@ -41,9 +60,9 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
 
