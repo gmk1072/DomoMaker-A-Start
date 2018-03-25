bb0136d28c772093e3dbb53ebb400313fb60981b
Gavin Keirstead <gmk1072@g.rit.edu>
2018-03-25 14:05:39 -0400

    domoD checking if it works on heroku

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -42,5 +42,18 @@ const makerPage = (req, res) => {
   });
 };
 
+const getDomos = (request, response) => {
+  const req = request;
+  const res = response;
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'an error occurred' });
+    }
+    return res.json({ domos: docs });
+  });
+};
+
 module.exports.makerPage = makerPage;
 module.exports.make = makeDomo;
+module.exports.getDomos = getDomos;
